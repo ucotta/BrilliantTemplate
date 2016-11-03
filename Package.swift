673e02fab541a702405e3e78befc4e48a06472c0
Ubaldo Cotta <ucotta@me.com>
2016-11-03 04:45:05 +0200

    instructions in package.swift

--- Package.swift
+++ Package.swift
@@ -8,6 +8,20 @@
 
 import PackageDescription
 
+print("---------------------------------------------------------------------------------")
+print("BrilliantTemplate depends libxml2, please, configure Xcode by reading the wiki:")
+print("\n\thttps://github.com/ucotta/BrilliantTemplate/wiki/Prerequisites-and-Setup")
+print("\nYep, you dont read docs, ok, do it:")
+print("\n\tbrew install libxml2")
+print("\nNow open xcode and change this settings in your project:")
+print("\n\tIn Other Linker flags in PROJECT:\n\t\tadd -lxml2")
+print("\tIn Header Search Paths in Kanna and your project: \n\t\tadd $(SDKROOT)/usr/include/libxml2")
+print("\tObjective-C Bridging Header in Kanna and your project: \n\t\tadd $PROJECT_DIR/Sources/Bridging-Header.h")
+print("\nCopy Bridging-Header.h from Packages/BrilliantTemplate-X.X/Sources into ")
+print("  your Sources path and add it to Xcode")
+print("---------------------------------------------------------------------------------")
+
+
 let package = Package(
 	name: "BrilliantTemplate",
 	targets: [],
