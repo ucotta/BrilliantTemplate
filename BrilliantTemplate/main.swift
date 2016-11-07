ac79a53d5297d83aeae9cf593dedf188f58345aa
Ubaldo Cotta <ucotta@me.com>
2016-11-07 14:28:03 +0200

    Initial Commit

---
+++ BrilliantTemplate/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BrilliantTemplate
+//
+//  Created by Ubaldo Cotta on 7/11/16.
+//  Copyright © 2016 Ubaldo Cotta. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
